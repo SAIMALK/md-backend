45a37babb077af8e2a353f12c7b3ec9fc15b6445
SAIMALK <115073479+SAIMALK@users.noreply.github.com>
2024-11-27 23:26:38 +0500

    Update authorRoute.js

--- routes/authorRoute.js
+++ routes/authorRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-
-import { getAuthor, getAuthorById } from "../controllers/authorController.js";
+import { protect } from '../middleware/authMiddleware.js';
+import checkObjectId from '../middleware/checkObjectId.js';
+import { getAuthor, getAuthorById , createStoryReview} from "../controllers/authorController.js";
 
 router.route("/").get(getAuthor);
 
 router.route("/:id").get(getAuthorById);
-
+router.route('/:id/reviews').post(protect, checkObjectId, createStoryReview);
 export default router;
